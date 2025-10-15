34b1fef346740043ab9c51c0b08cc9291cfbfede
Apissr <24111814053@mhs.unesa.ac.id>
2025-10-15 23:55:24 +0700

    update latest web build

--- android/app/src/main/kotlin/com/example/webruna/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.webruna
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/menu_item.dart
+++
@@ -1,7 +0,0 @@
-class MenuItem {
-  final String nama;
-  final String harga;
-  final String gambar;
-
-  MenuItem({required this.nama, required this.harga, required this.gambar});
-}
